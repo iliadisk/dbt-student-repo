028e96b38fcac1af78f4e4378fb1ea810adc8a48
Constantine Eliades <iliadisk.the@gmail.com>
2026-02-11 10:05:39 +0200

    Add src model configurations and create dim_hosts_cleansed and dim_listings_cleansed SQL files

---
+++ airbnb/models/dim/dim_hosts_cleansed.sql
@@ -0,0 +1,14 @@
+WITH src_hosts AS (
+    SELECT
+        * 
+    FROM 
+        {{ ref('src_hosts') }}
+) 
+SELECT
+    host_id,
+    NVL(host_name, 'Anonymous') AS host_name,
+    created_at,
+    updated_at,
+    is_superhost
+FROM
+    src_hosts
